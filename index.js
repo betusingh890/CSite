7d04a5742f7b4379e171a27a44522d24e95b481d
Prasoon Rajpoot <prasoonrajpoot@Prasoons-MacBook-Pro.local>
2022-01-18 22:49:49 +0530

    hello

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import express from "express";
-import path from "path";
 
-import path from 'path';
+import path from "path";
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename)
 
 import connectDB from "./sourceFiles/connectdb.js";
 import SignupFile from "./sourceFiles/signup.js";
