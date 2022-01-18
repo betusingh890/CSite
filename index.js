e0f8ee1d079e3b1d3ce57afca80b9bb994e17392
Prasoon Rajpoot <prasoonrajpoot@Prasoons-MacBook-Pro.local>
2022-01-18 22:58:57 +0530

    f

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.use("/login", LoginFile);
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static("frontend/build"));  
     app.get("*", (req,res) =>{
-        res.sendFile(path.resolve(__dirname, "/frontend/build", 'index.html'))
+        res.sendFile(path.resolve(__dirname,'frontend','build',         
+                      'index.html'));
     })  
 }
 
